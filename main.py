8c503a133bc20c8191586594b39cdc1ca2943629
Giuseppe Gullo <gfgullo@gmail.com>
2021-03-16 16:57:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
 from app import create_app
 
-app = create_app(os.environ.get("FLASK_ENVIRON") or "development")
-app.run()
+app = create_app("development")
+
+if __name__ == "__main__":
+    app.run()
